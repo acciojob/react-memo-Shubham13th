5f1f6e60fec55e6cdffb1553c7fe06153dc7081b
shubh <shubhamsharmajcc@gmail.com>
2026-02-01 23:45:24 +0530

    again

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,34 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+
+const App = () => {
+
+    const [task, setTask] = useState('');
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(task.length < 6){
+            alert('Task should be at least 6 characters long');
+            return;
+        }
+
+
+    }
+    return (
+        <div>
+            <h1>Task Management App with React Memo</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='Enter a task...' value={task} onChange={handleChange} />
+
+                <button type='submit'>New todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default App
 
